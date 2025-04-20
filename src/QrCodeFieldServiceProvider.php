2ff7a7641f0f92cb990ee946c5565ff691ba7f5e
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-04-19 23:33:52 -0300

    feat(package): add custom QR code field with service provider
    introduce QrCodeFieldServiceProvider to register package assets and views
    remove unused dependency 'timokoerber/laravel-qrcode-field'
    add initial asset file for the package

---
+++ src/QrCodeFieldServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace JeffersonGoncalves\Filament\QrCodeField;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class QrCodeFieldServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('filament-qrcode-field')
+            ->hasViews()
+            ->hasAssets();
+    }
+}
