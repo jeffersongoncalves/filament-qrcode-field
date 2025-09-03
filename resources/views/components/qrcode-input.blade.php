380199bd5814d63befceb006522665f75abe48a4
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-09-03 19:58:18 -0300

    feat(qrcode-input): refactor component to enhance flexibility and structure
    update QR code input component to use dynamic field wrapper, refine attributes and layout, and improve modularity for better maintainability.

--- resources/views/components/qrcode-input.blade.php
+++ resources/views/components/qrcode-input.blade.php
@@ -29,9 +29,14 @@
             'fi-revealable' => $isPasswordRevealable,
         ]);
 @endphp
-<div xmlns:x-filament="http://www.w3.org/1999/html"
-     x-load-js="['https://unpkg.com/html5-qrcode']"
-     x-data="{
+<x-dynamic-component
+    :component="$getFieldWrapperView()"
+    :field="$field"
+    :has-inline-label="$hasInlineLabel"
+>
+    <div xmlns:x-filament="http://www.w3.org/1999/html"
+         x-load-js="['https://unpkg.com/html5-qrcode']"
+         x-data="{
         html5QrcodeScanner: null,
         stopScanning() {
            if(!this.html5QrcodeScanner) {
@@ -59,94 +64,93 @@
             this.html5QrcodeScanner.render(this.onScanSuccess);
          }
      }"
->
-    <div class="grid gap-y-2">
-        <div class="flex items-center gap-x-3 justify-between">
-            <label for="{{ $getId() }}" class="fi-fo-field-wrp-label inline-flex items-center gap-x-3">
-                <span class="text-sm font-medium leading-6 text-gray-950 dark:text-white">
-                    {{ $getLabel() ?? 'Input Label' }}
-                    @if($isRequired())
-                        <sup class="text-danger-600 dark:text-danger-400 font-medium">*</sup>
-                    @endif
-                </span>
-            </label>
-        </div>
+    >
+        <div class="grid gap-y-2">
+            <x-slot
+                name="label"
+                @class([
+                    'sm:pt-1.5' => $hasInlineLabel,
+                ])
+            >
+                {{ $getLabel() }}
+            </x-slot>
 
-        <x-filament::input.wrapper :disabled="$isDisabled" :valid="! $errors->has($statePath)"
-                                   :attributes="prepare_inherited_attributes($extraAttributeBag)->class(['fi-fo-text-input'])">
-            <x-filament::input
-                type="text"
-                name="{{ $getName() }}"
-                id="{{ $getId() }}"
-                value="{{ $getState() }}"
-                placeholder="{{ $getPlaceholder() }}"
-                class="w-full pr-10"
-            />
+            <x-filament::input.wrapper :disabled="$isDisabled" :valid="! $errors->has($statePath)"
+                                       :attributes="prepare_inherited_attributes($extraAttributeBag)->class(['fi-fo-text-input'])">
+                <x-filament::input
+                    type="text"
+                    name="{{ $getName() }}"
+                    id="{{ $getId() }}"
+                    value="{{ $getState() }}"
+                    placeholder="{{ $getPlaceholder() }}"
+                    class="w-full pr-10"
+                />
 
-            <x-slot name="suffix">
-                <!-- Trigger Button for Filament Modal -->
-                <button type="button" @click="openScannerModal()" class="flex items-center pr-3 focus:outline-none"
-                        aria-label="Scan QrCode">
-                    @if($getExtraAttributes()['icon'] ?? null)
-                        <span class="text-gray-400 dark:text-gray-200">
+                <x-slot name="suffix">
+                    <!-- Trigger Button for Filament Modal -->
+                    <button type="button" @click="openScannerModal()" class="flex items-center pr-3 focus:outline-none"
+                            aria-label="Scan QrCode">
+                        @if($getExtraAttributes()['icon'] ?? null)
+                            <span class="text-gray-400 dark:text-gray-200">
                             <x-dynamic-component :component="$getExtraAttributes()['icon']" class="w-5 h-5"/>
                         </span>
-                    @else
-                        <svg class="w-5 h-5 text-gray-400 dark:text-gray-200" xmlns="http://www.w3.org/2000/svg"
-                             xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 16 16"
-                             fill="currentColor">
-                            <path fill="currentColor" d="M6 0h-6v6h6v-6zM5 5h-4v-4h4v4z"></path>
-                            <path fill="currentColor" d="M2 2h2v2h-2v-2z"></path>
-                            <path fill="currentColor" d="M0 16h6v-6h-6v6zM1 11h4v4h-4v-4z"></path>
-                            <path fill="currentColor" d="M2 12h2v2h-2v-2z"></path>
-                            <path fill="currentColor" d="M10 0v6h6v-6h-6zM15 5h-4v-4h4v4z"></path>
-                            <path fill="currentColor" d="M12 2h2v2h-2v-2z"></path>
-                            <path fill="currentColor" d="M2 7h-2v2h3v-1h-1z"></path>
-                            <path fill="currentColor" d="M7 9h2v2h-2v-2z"></path>
-                            <path fill="currentColor" d="M3 7h2v1h-2v-1z"></path>
-                            <path fill="currentColor" d="M9 12h-2v1h1v1h1v-1z"></path>
-                            <path fill="currentColor" d="M6 7v1h-1v1h2v-2z"></path>
-                            <path fill="currentColor" d="M8 4h1v2h-1v-2z"></path>
-                            <path fill="currentColor" d="M9 8v1h2v-2h-3v1z"></path>
-                            <path fill="currentColor" d="M7 6h1v1h-1v-1z"></path>
-                            <path fill="currentColor" d="M9 14h2v2h-2v-2z"></path>
-                            <path fill="currentColor" d="M7 14h1v2h-1v-2z"></path>
-                            <path fill="currentColor" d="M9 11h1v1h-1v-1z"></path>
-                            <path fill="currentColor" d="M9 3v-2h-1v-1h-1v4h1v-1z"></path>
-                            <path fill="currentColor" d="M12 14h1v2h-1v-2z"></path>
-                            <path fill="currentColor" d="M12 12h2v1h-2v-1z"></path>
-                            <path fill="currentColor" d="M11 13h1v1h-1v-1z"></path>
-                            <path fill="currentColor" d="M10 12h1v1h-1v-1z"></path>
-                            <path fill="currentColor" d="M14 10v1h1v1h1v-2h-1z"></path>
-                            <path fill="currentColor" d="M15 13h-1v3h2v-2h-1z"></path>
-                            <path fill="currentColor" d="M10 10v1h3v-2h-2v1z"></path>
-                            <path fill="currentColor" d="M12 7v1h2v1h2v-2h-2z"></path>
-                        </svg>
-                    @endif
-                </button>
-            </x-slot>
-        </x-filament::input.wrapper>
+                        @else
+                            <svg class="w-5 h-5 text-gray-400 dark:text-gray-200" xmlns="http://www.w3.org/2000/svg"
+                                 version="1.1" viewBox="0 0 16 16"
+                                 fill="currentColor">
+                                <path fill="currentColor" d="M6 0h-6v6h6v-6zM5 5h-4v-4h4v4z"></path>
+                                <path fill="currentColor" d="M2 2h2v2h-2v-2z"></path>
+                                <path fill="currentColor" d="M0 16h6v-6h-6v6zM1 11h4v4h-4v-4z"></path>
+                                <path fill="currentColor" d="M2 12h2v2h-2v-2z"></path>
+                                <path fill="currentColor" d="M10 0v6h6v-6h-6zM15 5h-4v-4h4v4z"></path>
+                                <path fill="currentColor" d="M12 2h2v2h-2v-2z"></path>
+                                <path fill="currentColor" d="M2 7h-2v2h3v-1h-1z"></path>
+                                <path fill="currentColor" d="M7 9h2v2h-2v-2z"></path>
+                                <path fill="currentColor" d="M3 7h2v1h-2v-1z"></path>
+                                <path fill="currentColor" d="M9 12h-2v1h1v1h1v-1z"></path>
+                                <path fill="currentColor" d="M6 7v1h-1v1h2v-2z"></path>
+                                <path fill="currentColor" d="M8 4h1v2h-1v-2z"></path>
+                                <path fill="currentColor" d="M9 8v1h2v-2h-3v1z"></path>
+                                <path fill="currentColor" d="M7 6h1v1h-1v-1z"></path>
+                                <path fill="currentColor" d="M9 14h2v2h-2v-2z"></path>
+                                <path fill="currentColor" d="M7 14h1v2h-1v-2z"></path>
+                                <path fill="currentColor" d="M9 11h1v1h-1v-1z"></path>
+                                <path fill="currentColor" d="M9 3v-2h-1v-1h-1v4h1v-1z"></path>
+                                <path fill="currentColor" d="M12 14h1v2h-1v-2z"></path>
+                                <path fill="currentColor" d="M12 12h2v1h-2v-1z"></path>
+                                <path fill="currentColor" d="M11 13h1v1h-1v-1z"></path>
+                                <path fill="currentColor" d="M10 12h1v1h-1v-1z"></path>
+                                <path fill="currentColor" d="M14 10v1h1v1h1v-2h-1z"></path>
+                                <path fill="currentColor" d="M15 13h-1v3h2v-2h-1z"></path>
+                                <path fill="currentColor" d="M10 10v1h3v-2h-2v1z"></path>
+                                <path fill="currentColor" d="M12 7v1h2v1h2v-2h-2z"></path>
+                            </svg>
+                        @endif
+                    </button>
+                </x-slot>
+            </x-filament::input.wrapper>
 
-    </div>
-
-    <!-- Filament Modal for QrCode Scanner -->
-    <x-filament::modal id="qrcode-scanner-modal-{{ $getName() }}" width="lg" :close-by-clicking-away="false">
-        <x-slot name="header">
-            <h2 class="text-lg font-semibold">
-                Scan {{ $getLabel() ?? 'QrCode' }}
-            </h2>
-        </x-slot>
-
-        <div class="p-4">
-            <div id="scanner-container">
-                <div id="reader-{{ $getName() }}" width="600px" height="600px"></div>
-            </div>
         </div>
 
-        <x-slot name="footer">
-            <x-filament::button @click="closeScannerModal()" color="danger">
-                Close
-            </x-filament::button>
-        </x-slot>
-    </x-filament::modal>
-</div>
+        <!-- Filament Modal for QrCode Scanner -->
+        <x-filament::modal id="qrcode-scanner-modal-{{ $getName() }}" width="lg" :close-by-clicking-away="false">
+            <x-slot name="header">
+                <h2 class="text-lg font-semibold">
+                    Scan {{ $getLabel() ?? 'QrCode' }}
+                </h2>
+            </x-slot>
+
+            <div class="p-4">
+                <div id="scanner-container">
+                    <div id="reader-{{ $getName() }}" width="600px" height="600px"></div>
+                </div>
+            </div>
+
+            <x-slot name="footer">
+                <x-filament::button @click="closeScannerModal()" color="danger">
+                    Close
+                </x-filament::button>
+            </x-slot>
+        </x-filament::modal>
+    </div>
+</x-dynamic-component>
