cafce430c0a739cf5abd15c9ef8b0b9f2a2ad062
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-12-28 15:31:11 -0300

    refactor(config): replace Width enum with string value for modal size
    simplify modal width definition by using a string value instead of the Width enum

--- config/filament-qrcode-field.php
+++ config/filament-qrcode-field.php
@@ -1,11 +1,9 @@
 <?php
 
-use Filament\Support\Enums\Width;
-
 return [
     'asset_js' => 'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js',
     'modal' => [
-        'width' => Width::Large,
+        'width' => 'lg',
     ],
     'reader' => [
         'width' => '600px',
